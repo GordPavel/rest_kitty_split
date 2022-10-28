7ddd8f02fb834df6fe45460b421982fb28f8e906
Гордеев Павел Васильевич <p.v.gordeev@tinkoff.ru>
2022-10-29 01:57:18 +0400

    add README.md

--- src/main/kotlin/ru/sau/kitty_split/split/service/SplitService.kt
+++ src/main/kotlin/ru/sau/kitty_split/split/service/SplitService.kt
@@ -1,13 +1,11 @@
 package ru.sau.kitty_split.split.service
 
-import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Service
 
 interface SplitService {
     fun splitBill(inputData: InputData): OutputData
 }
 
-@Primary
 @Service
 class SplitServiceStub : SplitService {
     override fun splitBill(inputData: InputData): OutputData {
