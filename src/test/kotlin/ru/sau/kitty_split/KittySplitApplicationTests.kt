20b056d40dfde942c4a54fc1b415de9ed99fd532
GordPavel <pvgord@icloud.com>
2023-02-21 19:03:44 +0400

    fix tests

--- src/test/kotlin/ru/sau/kitty_split/KittySplitApplicationTests.kt
+++ src/test/kotlin/ru/sau/kitty_split/KittySplitApplicationTests.kt
@@ -1,15 +1,16 @@
 package ru.sau.kitty_split
 
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
-//@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest
 class KittySplitApplicationTests {
 
     private lateinit var application: KittySplitApplication
 
-    //    @Test
+    @Test
     fun contextLoads() {
         assertNotNull(application)
     }
